b1dd11ccb5123a096ed5eaebdb50a25107e3d5f9
Ismaila DIALLO <idiallo@assurone-group.com>
2020-10-14 17:39:38 +0200

    first commit

---
+++ crudAPI/Models/TodoItem.cs
@@ -0,0 +1,7 @@
+﻿public class TodoItem
+{
+    public long Id { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public bool published { get; set; }
+}
